40f89b47dfdb6969ee40b85fbd9830a311c025a9
meganpet <javierguerra987654@gmail.com>
2026-08-24 21:12:38 +0200

    feat: add JARVIS loading state

---
+++ jarvis-business-os/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className="grid min-h-screen place-items-center bg-[#05070a] text-cyan-200">
+      <div className="text-xs tracking-[0.35em]">J.A.R.V.I.S. / INITIALIZING</div>
+    </main>
+  );
+}
